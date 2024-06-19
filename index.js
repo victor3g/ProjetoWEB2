de43104cfbe9578071527fee6759257bd7892dd6
Victor Gomes Cavalcante <victorgomessc@gmail.com>
2024-06-19 01:44:51 -0300

    adicionando listagem dos produtos na pagina home

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+//Página home - listagem dos produtos
+app.get('/home', async (req, res) => {
+  try {
+    const result = await conexao.query(`
+      SELECT 
+        produtos.id AS id,
+        produtos.nome AS nome,
+        produtos.descricao AS descricao,
+        produtos.quantidade AS quantidade,
+        produtos.preco_unitario AS preco_unitario,
+        categorias.id AS categoria_id,
+        categorias.nome AS categoria,
+        fornecedores.id AS fornecedor_id,
+        fornecedores.nome AS fornecedor,
+        produtos.status AS status
+      FROM 
+        produtos
+      INNER JOIN categorias ON produtos.categoria_id = categorias.id
+      INNER JOIN fornecedores ON produtos.fornecedor_id = fornecedores.id
+    `);
+
+    const itens = result.rows;
+    const categoriasResult = await conexao.query('SELECT id, nome FROM categorias ORDER BY nome');
+    const fornecedoresResult = await conexao.query('SELECT id, nome FROM fornecedores ORDER BY nome');
+
+    const categorias = categoriasResult.rows;
+    const fornecedores = fornecedoresResult.rows;
+
+    res.render('home', { itens, categorias, fornecedores });
+  }catch(e){
+    console.log(e)
+  }
+});
+
 //Rodando o servidor
 app.listen(3000, function(){
   console.log('O Servidor está online na porta 3000');
